3bd5d18b6d030ad9f65c738bfa9ff276ad61ac14
Seb <github@seb.exse.net>
2012-03-31 04:48:39 +0200

    removed

--- lichtorgel/keyboard.h
+++
@@ -1,8 +0,0 @@
-typedef struct {
-	unsigned char type, x, y;
-} KeyboardEvent;
-
-int		keyboard_init();
-void	keyboard_kill();
-int		keyboard_poll(KeyboardEvent* e);
-
--- lichtorgel/main.h
+++
@@ -1,7 +0,0 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-void pixel(uint8_t,uint8_t,uint8_t);
-void write_escaped(uint8_t);
-
-#endif
